b404db34822f974f6a5a3cde2f28b185f9bf2c61
Oafish1 <themathmaster3000@gmail.com>
2021-06-15 21:28:23 -0500

    Downgrade and Fixes for Title Detection and AnkiConnect Interface

--- src/background.js
+++ src/background.js
@@ -1,14 +1,14 @@
 const enableDisableTab = (id, url) => {
   const re = /(http|https):\/\/(www.)?youtube\.com\\*/
   if (re.test(url)) {
-    chrome.action.enable(id)
+    chrome.browserAction.enable(id)
   } else {
-    chrome.action.disable(id)
+    chrome.browserAction.disable(id)
   }
 }
 
 chrome.tabs.onActivated.addListener((tab) => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
     const [activeTab] = tabs
     const { url, id } = activeTab
     enableDisableTab(id, url)
@@ -16,7 +16,7 @@ chrome.tabs.onActivated.addListener((tab) => {
 })
 
 chrome.tabs.onUpdated.addListener((id, changeInfo, tab) => {
-  if (changeInfo.status === 'complete') {
-    enableDisableTab(id, tab.url)
-  }
+  if (changeInfo.status == "complete") {
+      enableDisableTab(id, tab.url)
+    }
 })
